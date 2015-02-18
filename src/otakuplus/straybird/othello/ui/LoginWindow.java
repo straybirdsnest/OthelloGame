6373de2a754ea215b7c1b2706c3631508208a810
stryabirdsnest <straybirdsnest@gmail.com>
2015-02-18 23:28:19 +0800

    测试SWT的图片功能。

--- src/otakuplus/straybird/othello/ui/LoginWindow.java
+++ src/otakuplus/straybird/othello/ui/LoginWindow.java
@@ -1,8 +1,13 @@
 package otakuplus.straybird.othello.ui;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
@@ -11,22 +16,39 @@ import org.eclipse.swt.widgets.Text;
 public class LoginWindow {
 
 	public static void main(String[] args) {
-		Display display = new Display();
+		final Display display = new Display();
 		Shell loginWindow = new Shell(display);
+
 		loginWindow.setText("Login");
 		loginWindow.setSize(480, 320);
 		GridLayout loginWindowLayout = new GridLayout();
-		loginWindowLayout.numColumns = 2;
+		loginWindowLayout.numColumns = 5;
 		loginWindow.setLayout(loginWindowLayout);
-		Label userNameLabel = new Label(loginWindow, SWT.NONE);
+
+		Canvas canvas = new Canvas(loginWindow, SWT.NONE);
+		GridData canvasGridData = new GridData(GridData.FILL_HORIZONTAL);
+		canvasGridData.horizontalSpan = 5;
+		canvas.setLayoutData(canvasGridData);
+		canvas.addPaintListener(new PaintListener() {
+
+			@Override
+			public void paintControl(PaintEvent e) {
+				Image image = new Image(display, "89.png");
+				e.gc.drawImage(image, 0, 0, 200, 50, 0, 0, 200, 50);
+				image.dispose();
+			}
+		});
+
+		Label userNameLabel = new Label(loginWindow, SWT.CENTER);
 		userNameLabel.setText("UserName: ");
-		Text userNameText = new Text(loginWindow, SWT.NONE);
-		Label passWordLabel = new Label(loginWindow, SWT.NONE);
+		userNameLabel.setLayoutData(new GridData());
+		Text userNameText = new Text(loginWindow, SWT.CENTER);
+		Label passWordLabel = new Label(loginWindow, SWT.CENTER);
 		passWordLabel.setText("PassWord: ");
-		Text passWordText = new Text(loginWindow, SWT.NONE);
-		Button loginButton = new Button(loginWindow, SWT.PUSH);
+		Text passWordText = new Text(loginWindow, SWT.CENTER);
+		Button loginButton = new Button(loginWindow, SWT.CENTER | SWT.PUSH);
 		loginButton.setText("Login");
-		Button canceButton = new Button(loginWindow, SWT.PUSH);
+		Button canceButton = new Button(loginWindow, SWT.CENTER | SWT.PUSH);
 		canceButton.setText("Cance");
 
 		loginWindow.open();
