6609890c3a1b902ef0dea8961a4b9ec44db3dfc9
stryabirdsnest <straybirdsnest@gmail.com>
2015-02-19 23:48:24 +0800

    利用SWT绘制简单的2D游戏界面。

--- src/otakuplus/straybird/othello/ui/LoginWindow.java
+++ src/otakuplus/straybird/othello/ui/LoginWindow.java
@@ -10,53 +10,86 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Link;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
 public class LoginWindow {
+	protected Shell shell;
 
-	public static void main(String[] args) {
-		final Display display = new Display();
-		Shell loginWindow = new Shell(display);
-
-		loginWindow.setText("Login");
-		loginWindow.setSize(480, 320);
-		GridLayout loginWindowLayout = new GridLayout();
-		loginWindowLayout.numColumns = 5;
-		loginWindow.setLayout(loginWindowLayout);
-
-		Canvas canvas = new Canvas(loginWindow, SWT.NONE);
-		GridData canvasGridData = new GridData(GridData.FILL_HORIZONTAL);
-		canvasGridData.horizontalSpan = 5;
-		canvas.setLayoutData(canvasGridData);
-		canvas.addPaintListener(new PaintListener() {
-
-			@Override
-			public void paintControl(PaintEvent e) {
-				Image image = new Image(display, "89.png");
-				e.gc.drawImage(image, 0, 0, 200, 50, 0, 0, 200, 50);
-				image.dispose();
-			}
-		});
-
-		Label userNameLabel = new Label(loginWindow, SWT.CENTER);
-		userNameLabel.setText("UserName: ");
-		userNameLabel.setLayoutData(new GridData());
-		Text userNameText = new Text(loginWindow, SWT.CENTER);
-		Label passWordLabel = new Label(loginWindow, SWT.CENTER);
-		passWordLabel.setText("PassWord: ");
-		Text passWordText = new Text(loginWindow, SWT.CENTER);
-		Button loginButton = new Button(loginWindow, SWT.CENTER | SWT.PUSH);
-		loginButton.setText("Login");
-		Button canceButton = new Button(loginWindow, SWT.CENTER | SWT.PUSH);
-		canceButton.setText("Cance");
-
-		loginWindow.open();
-		while (!loginWindow.isDisposed()) {
+	public void open() {
+		final Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		while (!shell.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
 		}
 		display.dispose();
+	}
+
+	protected void createContents() {
+		shell = new Shell(SWT.SHELL_TRIM);
+		shell.setText("Login");
+		//shell.setSize(480, 320);
+		GridLayout loginWindowLayout = new GridLayout();
+		loginWindowLayout.numColumns = 7;
+		loginWindowLayout.makeColumnsEqualWidth = false;
+		shell.setLayout(loginWindowLayout);
+
+		Canvas canvas = new Canvas(shell, SWT.NONE);
+		GridData canvasGridData = new GridData(GridData.FILL_BOTH);
+		canvasGridData.horizontalSpan = 7;
+		canvas.setLayoutData(canvasGridData);
+		canvas.addPaintListener(new PaintListener() {
+			@Override
+			public void paintControl(PaintEvent e) {
+				Image image = new Image(Display.getDefault(), "Othello.png");
+				e.gc.drawImage(image, 0, 0, 391, 192, 0, 0, 391, 192);
+				image.dispose();
+			}
+		});
+
+		new Label(shell, SWT.CENTER).setLayoutData(new GridData(SWT.LEFT,
+				SWT.CENTER, false, false, 1, 1));
+
+		Label userNameLabel = new Label(shell, SWT.CENTER);
+		userNameLabel.setText("UserName: ");
+		userNameLabel.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false,
+				false, 1, 1));
+		Text userNameText = new Text(shell, SWT.CENTER);
+		userNameText.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false,
+				false, 3, 1));
+		Link registerLink = new Link(shell, SWT.CENTER);
+		registerLink.setText("Register");
+		new Label(shell, SWT.CENTER).setLayoutData(new GridData(SWT.LEFT,
+				SWT.CENTER, false, false, 1, 1));
+		new Label(shell, SWT.CENTER).setLayoutData(new GridData(SWT.LEFT,
+				SWT.CENTER, false, false, 1, 1));
+
+		Label passWordLabel = new Label(shell, SWT.CENTER);
+		passWordLabel.setText("PassWord: ");
+		Text passWordText = new Text(shell, SWT.CENTER);
+		passWordText.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false,
+				false, 3, 1));
+		Link forgetLink = new Link(shell, SWT.CENTER);
+		forgetLink.setText("Forget Password");
+		new Label(shell, SWT.CENTER).setLayoutData(new GridData(SWT.LEFT,
+				SWT.CENTER, false, false, 1, 1));
+
+		Button loginButton = new Button(shell, SWT.CENTER | SWT.PUSH);
+		loginButton.setText("Login");
+		Button canceButton = new Button(shell, SWT.CENTER | SWT.PUSH);
+		canceButton.setText("Cance");
+	}
+
+	public static void main(String[] args) {
+		try {
+			LoginWindow loginWindow = new LoginWindow();
+			loginWindow.open();
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
 	}
 }
--- src/otakuplus/straybird/othello/ui/OthelloGameWindow.java
+++ src/otakuplus/straybird/othello/ui/OthelloGameWindow.java
@@ -1,22 +1,124 @@
 package otakuplus.straybird.othello.ui;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
-public class OthelloGameWindow {
+import otakuplus.straybird.othello.model.ChessBoard;
+import otakuplus.straybird.othello.model.Chessman;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Display display = new Display();
-		Shell othelloGameWindow = new Shell(display);
-		
-		othelloGameWindow.open();
-		while (!othelloGameWindow.isDisposed()) {
+public class OthelloGameWindow {
+	protected Shell shell;
+	protected ChessBoard chessBoard;
+
+	public OthelloGameWindow() {
+		if (chessBoard == null) {
+			chessBoard = new ChessBoard();
+			chessBoard.searchSuggestedChessmanPosition();
+		}
+	}
+
+	protected void updateGame() {
+		chessBoard.searchSuggestedChessmanPosition();
+	}
+
+	public void open() {
+		final Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		while (!shell.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
 		}
 		display.dispose();
+	}
+
+	protected void createContents() {
+		shell = new Shell(SWT.SHELL_TRIM);
+		shell.setText("OthelloGame");
+		Image image = new Image(Display.getDefault(), "Othello.png");
+		shell.setBackgroundImage(image);
+		image.dispose();
+		GridLayout othellGameLayout = new GridLayout();
+		shell.setLayout(othellGameLayout);
+
+		Canvas canvas = new Canvas(shell, SWT.NONE);
+		GridData canvasGridData = new GridData(GridData.FILL_BOTH);
+		canvas.setLayoutData(canvasGridData);
+		canvas.addPaintListener(new PaintListener() {
+			@Override
+			public void paintControl(PaintEvent e) {
+				Image image = new Image(Display.getDefault(), "Othello.png");
+				e.gc.drawImage(image, 0, 0, 391, 192, 0, 0, 391, 192);
+
+				// drawing chessboard
+				e.gc.setForeground(Display.getDefault().getSystemColor(
+						SWT.COLOR_BLACK));
+				e.gc.setBackground(Display.getDefault().getSystemColor(
+						SWT.COLOR_GRAY));
+				e.gc.drawRectangle(0, 0, 320, 320);
+				e.gc.fillRectangle(1, 1, 319, 319);
+
+				int i = 0, chessmanStat = 0;
+				for (i = 1; i < 8; i++) {
+					e.gc.drawLine(40 * i, 0, 40 * i, 320);
+					e.gc.drawLine(0, 40 * i, 320, 40 * i);
+				}
+
+				// drawing chessmen
+				i = 0;
+				for (i = 0; i < 64; i++) {
+					chessmanStat = chessBoard.getChessman(i / 8, i % 8)
+							.getChessman();
+					if (chessmanStat == Chessman.CHESSMAN_BLACK) {
+						e.gc.setBackground(Display.getDefault().getSystemColor(
+								SWT.COLOR_BLACK));
+						e.gc.drawOval((i % 8) * 40 + 4, (i / 8) * 40 + 4, 32,
+								32);
+						e.gc.fillOval((i % 8) * 40 + 5, (i / 8) * 40 + 5, 31,
+								31);
+					} else if (chessmanStat == Chessman.CHESSMAN_WHITE) {
+						e.gc.setBackground(Display.getDefault().getSystemColor(
+								SWT.COLOR_WHITE));
+						e.gc.drawOval((i % 8) * 40 + 4, (i / 8) * 40 + 4, 32,
+								32);
+						e.gc.fillOval((i % 8) * 40 + 5, (i / 8) * 40 + 5, 31,
+								31);
+					}
+				}
+
+				// drawing suggested position
+				int suggestedPosition[] = chessBoard.getSuggestedPosition();
+				e.gc.setBackground(Display.getDefault().getSystemColor(
+						SWT.COLOR_CYAN));
+				for (i = 0; i < suggestedPosition.length; i++) {
+					if (suggestedPosition[i] == 1) {
+						e.gc.drawRectangle((i % 8) * 40 + 4, i / 8 * 40 + 4,
+								32, 32);
+						e.gc.fillRectangle((i % 8) * 40 + 5, i / 8 * 40 + 5,
+								31, 31);
+					}
+				}
+
+				image.dispose();
+			}
+		});
+	}
+
+	public static void main(String[] args) {
+		try {
+			OthelloGameWindow othelloGameWindow = new OthelloGameWindow();
+			othelloGameWindow.open();
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
 	}
 
 }
