6ec1490e8cf6761404f8af57f219e5333dd2584b
stryabirdsnest <straybirdsnest@gmail.com>
2015-03-17 20:38:06 +0800

    修改OthelloGameWindow界面组件使其类似QQ五子棋界面

--- src/otakuplus/straybird/othellogame/ui/OthelloGameWindow.java
+++ src/otakuplus/straybird/othellogame/ui/OthelloGameWindow.java
@@ -1,13 +1,22 @@
 package otakuplus.straybird.othellogame.ui;
 
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnPixelData;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.TableLayout;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
@@ -15,6 +24,8 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.Text;
 
 import otakuplus.straybird.othellogame.ApplicationState;
 import otakuplus.straybird.othellogame.MainApplication;
@@ -31,6 +42,14 @@ public class OthelloGameWindow {
 	protected Label blackLabel;
 	protected Label whiteTimer;
 	protected Label blackTimer;
+
+	protected Canvas chessBoardCanvas;
+	protected Text gameChat;
+	protected Text messageText;
+
+	protected Button takeBackButton;
+	protected Button drawButton;
+	protected Button giveUpButton;
 
 	protected ChessBoard chessBoard;
 
@@ -50,15 +69,15 @@ public class OthelloGameWindow {
 		shell.setLocation((Display.getDefault().getBounds().width - 400) / 2,
 				(Display.getDefault().getBounds().height - 400) / 2);
 		GridLayout othellGameLayout = new GridLayout();
-		othellGameLayout.numColumns = 5;
+		othellGameLayout.numColumns = 10;
 		shell.setLayout(othellGameLayout);
 
-		final Canvas canvas = new Canvas(shell, SWT.NONE);
+		chessBoardCanvas = new Canvas(shell, SWT.NONE);
 		GridData canvasGridData = new GridData(GridData.FILL_BOTH);
 		canvasGridData.widthHint = 321;
 		canvasGridData.heightHint = 321;
 		canvasGridData.verticalSpan = 4;
-		canvas.setLayoutData(canvasGridData);
+		chessBoardCanvas.setLayoutData(canvasGridData);
 
 		Label whiteUserlabel = new Label(shell, SWT.CENTER);
 		whiteUserlabel.setText("白方");
@@ -72,7 +91,7 @@ public class OthelloGameWindow {
 		blackTimer = new Label(shell, SWT.CENTER);
 		blackTimer.setText("15:00");
 
-		canvas.addPaintListener(new PaintListener() {
+		chessBoardCanvas.addPaintListener(new PaintListener() {
 			@Override
 			public void paintControl(PaintEvent e) {
 				// drawing chessboard
@@ -120,7 +139,7 @@ public class OthelloGameWindow {
 			}
 		});
 
-		canvas.addMouseListener(new MouseListener() {
+		chessBoardCanvas.addMouseListener(new MouseListener() {
 
 			@Override
 			public void mouseDoubleClick(MouseEvent e) {
@@ -148,13 +167,57 @@ public class OthelloGameWindow {
 				blackLabel.redraw();
 				whiteLabel.redraw();
 
-				canvas.redraw();
+				chessBoardCanvas.redraw();
 			}
 
 			@Override
 			public void mouseUp(MouseEvent e) {
 			}
 
+		});
+		takeBackButton = new Button(shell, SWT.PUSH);
+		GridData takeBackGridData = new GridData();
+		takeBackGridData.horizontalSpan = 3;
+		takeBackButton.setText("悔棋");
+		drawButton = new Button(shell, SWT.PUSH);
+		GridData drawGridData = new GridData();
+		drawGridData.horizontalSpan = 3;
+		drawButton.setText("和棋");
+		giveUpButton = new Button(shell, SWT.PUSH);
+		GridData giveUpGridData = new GridData();
+		giveUpGridData.horizontalSpan = 3;
+		giveUpButton.setText("认输");
+
+		gameChat = new Text(shell, SWT.MULTI | SWT.V_SCROLL);
+		GridData gameHallGridData = new GridData();
+		gameHallGridData.horizontalSpan = 3;
+		gameHallGridData.widthHint = 400;
+		gameHallGridData.heightHint = 200;
+		gameChat.setLayoutData(gameHallGridData);
+
+		messageText = new Text(shell, SWT.NONE);
+		GridData messageTextGridData = new GridData();
+		messageTextGridData.horizontalSpan = 2;
+		messageTextGridData.widthHint = 150;
+		messageText.setText("请输入聊天信息");
+		messageText.setLayoutData(messageTextGridData);
+
+		Button sendMessageButton = new Button(shell, SWT.PUSH);
+		GridData sendMessageGridData = new GridData();
+		sendMessageGridData.horizontalSpan = 1;
+		sendMessageButton.setLayoutData(sendMessageGridData);
+		sendMessageButton.setText("发送");
+		sendMessageButton.addSelectionListener(new SelectionListener() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				sendMessage();
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+
+			}
 		});
 
 		shell.addListener(SWT.Close, new Listener() {
@@ -196,6 +259,14 @@ public class OthelloGameWindow {
 
 	public void show() {
 		shell.setVisible(true);
+	}
+
+	public void sendMessage() {
+
+	}
+
+	public void destoryResource() {
+
 	}
 
 	public static void main(String[] args) {
